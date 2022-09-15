4edd844cd60dfb1b169df7dfd547c695cc0c712f
flash-of-genius <amhdaar@gmail.com>
2022-09-15 15:12:10 +0100

    src\App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
 
 export default App;
-
